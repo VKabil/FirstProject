2753248c85ee15460b30d6e98c1e159b78c97998
VKabil <kabilvkabil@gmail.com>
2023-12-22 19:45:48 +0530

    add

--- dummy.py
+++ dummy.py
@@ -1,3 +1,10 @@
-def dummy(a, b):
-  return a+b
-print(dummy(23, 30))
+nums = [0,1,4,6,7,10]
+diff = 3
+count =0
+for i in range(len(nums)):
+    for j in range(len(nums)):
+        for k in range(len(nums)):
+            if(nums[j] - nums[i] == diff) and (nums[k] - nums[j] == diff):
+                count +=1 
+
+print(count)
